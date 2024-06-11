5bfc7a33075a34286f259d2da167303b1e0d04e1
Prajwal K <prajwalk@Prajwals-Laptop.local>
2024-06-11 19:40:13 +0530

    added (dashboard) routes and userAppBar

---
+++ apps/user-app/app/components/UserAppBar.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Appbar } from "@repo/ui/appbar"
+import { useRouter } from "next/navigation";
+
+const UserAppBar = () =>{
+    const session = useSession();  
+    const router = useRouter();
+
+    return (
+   <div>
+      <Appbar onSignin={signIn} onSignout={async () => {
+        await signOut();
+        router.push("/api/auth/signin");
+      }} user={session?.data?.user} />
+   </div>
+    )
+}
+
+export default UserAppBar;    
